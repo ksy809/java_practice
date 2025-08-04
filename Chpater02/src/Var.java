b4418bd3b2951d833b01dda10e3ad3a505380ef8
ksy809 <ss741219@daum.net>
2025-08-04 22:42:10 +0900

    20250804 자바 복습

---
+++ Chpater02/src/Var.java
@@ -0,0 +1,15 @@
+
+public class Var {
+
+	public static void main(String[] args) {
+		var price = 200;
+		var name = "kitea";
+		var pi = 3.14;
+		
+		System.out.println("pirce = " + (price + 1000));
+		System.out.println("name = " + name);
+		System.out.println("pi = " + pi);
+
+	}
+
+}
